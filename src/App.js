550ed9b2ce4a0c4a380b63e073e359c41a60f54b
Arafin Ahmed <arafin2021@gmail.com>
2023-08-11 02:21:31 +0600

    hi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          From Arafin Ahmed. Deploye through Github Actions. <br />
         </p>
         <a
           className="App-link"
